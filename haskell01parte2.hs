dd31b416ccfe2d8b74ac82be8ecbb0d14875e2b7
miguelwisneski <85126042+miguelwisneski@users.noreply.github.com>
2021-06-08 09:45:29 -0300

    Update haskell01parte2.hs

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,2 +1,65 @@
 -- Prática 01 de Haskell - Parte 2
--- Nome: 
+-- Nome: Miguel Antonio Welter Wisneski
+
+--1
+htmlItem :: String -> String
+htmlItem x = "<li>" ++ x ++ "<li>"
+
+itemize :: [String] -> [String]
+itemize str1 = (map (htmlItem) str1 ) 
+
+--2
+isVowel :: Char -> Bool
+isVowel c = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' 
+
+onlyVowels :: String -> String
+onlyVowels str1 = (filter (isVowel) str1)
+
+--3
+isElderly :: Int -> Bool
+isElderly i = i > 65
+
+onlyElderly :: [Int] -> [Int]
+onlyElderly idade = (filter (isElderly) idade)
+
+--4
+isLongWord :: String -> Bool 
+isLongWord s = length s > 10
+
+onlyLongWords :: [String] -> [String]
+onlyLongWords longword = (filter (isLongWord) longword)
+
+--5
+onlyPar :: Int -> Bool
+onlyPar x = mod x 2 == 0
+
+onlyEven :: [Int] -> [Int]
+onlyEven x = (filter (onlyPar) x)
+
+--6
+between60and80 :: Int -> Bool
+between60and80 x = x >= 60 && x <= 80
+
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 x = (filter (between60and80) x)
+
+--7
+spaces :: Char -> Bool
+spaces x = x == ' '
+
+countSpaces :: String -> String
+countSpaces x = (filter (spaces) x)
+
+--8
+circleArea :: Float -> Float
+circleArea x = pi * x^2
+
+calcAreas :: [Float] -> [Float]
+calcAreas str1 = (map (circleArea) str1 ) 
+
+--9
+isChar :: Char -> Char -> Bool
+isChar c1 c2 = c1 == c2
+
+charFound :: Char -> String -> Bool
+charFound char string = length (filter (isChar char) string)>0
